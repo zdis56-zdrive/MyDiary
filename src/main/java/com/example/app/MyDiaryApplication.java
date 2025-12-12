fc13ae6ee19ce757fa4d4653c07a3454e8ebe3a0
Muraoka <muraoka@example.com>
2025-12-12 10:27:01 +0900

    init

---
+++ src/main/java/com/example/app/MyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiaryApplication.class, args);
+	}
+
+}
