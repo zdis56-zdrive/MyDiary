205a9de1c1f9016dfeeebfdf781b38cc3348ff65
Muraoka <muraoka@example.com>
2025-12-12 10:59:23 +0900

    Authorクラスの作成

---
+++ src/main/java/com/example/app/domain/Author.java
@@ -0,0 +1,16 @@
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class Author {
+	
+	private Integer id;
+	private String name;
+	private String loginId;
+	private String loginPass;
+
+}
+
+
+
